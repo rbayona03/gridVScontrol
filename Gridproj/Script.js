8da59dfecb0acad4a734f3c1aca7f3e04302a462
Robert bayona <69874938+rbayona03@users.noreply.github.com>
2021-03-24 18:09:23 -0400

    Add files via upload

---
+++ Gridproj/Script.js
@@ -0,0 +1,34 @@
+
+
+function slideOver(){
+
+    let ProImg = document.querySelector('.project1');
+    let Slider1 = document.querySelector('.slider1');
+
+
+    Slider1.style.width = "500px"
+    Slider1.style.opacity = "1"
+    Slider1.style.zindex = "auto"
+}
+
+function slideOver2(){
+
+    let ProImg = document.querySelector('.project2');
+    let Slider2 = document.querySelector('.slider2');
+
+
+    Slider2.style.width = "500px"
+    Slider2.style.opacity = "1"
+    Slider2.style.zindex = "auto"
+}
+
+function slideOver3(){
+
+    let ProImg = document.querySelector('.project3');
+    let Slider3 = document.querySelector('.slider3');
+
+
+    Slider3.style.width = "500px"
+    Slider3.style.opacity = "1"
+    Slider3.style.zindex = "auto"
+}
